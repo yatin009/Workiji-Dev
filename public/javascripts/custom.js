8f1b3c13f5672608a7e7cc26fe1e260f93cd31b9
Yatin Sharma <yatin.sharma1991@gmail.com>
2017-11-04 22:08:00 -0400

    Ui changes

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -138,80 +138,78 @@ $(document).ready(function(){
 //------------------------------------------------------------------------------------
 //						REGISTRATION FORM VALIDATION'S SETTINGS
 //------------------------------------------------------------------------------------		  
-//     $('#register_form').validate({
-//         onfocusout: false,
-//         onkeyup: false,
-//         rules: {
-//             name: "required",
-//             email: {
-//                 required: true,
-//                 email: true
-//             },
-//             address: "required",
-//             contactNumber: {
-//      			required: true,
-//       			minlength: 8
-//     		}
-//         },
-//         errorPlacement: function(error, element) {
-//             error.insertAfter(element);
-//         },
-//         messages: {
-//             name: "What's your name?",
-//             email: {
-//                 required: "What's your email?",
-//                 email: "Please, enter a valid email"
-//             },
-//             address: "What's your address?",
-//             contactNumber: {
-//       			required: "What's your contact?",
-//       			minlength: jQuery.format("At least {0} characters")
-//     		}
-//         },
-//
-//         highlight: function(element) {
-//             $(element)
-//             .text('').addClass('error')
-//         },
-//
-//         success: function(element) {
-//             element
-//             .text('').addClass('valid')
-//
-//             $.ajax({
-//                 url: 'http://localhost:3000/register/organization',
-//                 type: 'POST',
-//                 data: {
-//                     // newsletter_email: $('#subscribe_email').val()
-//                 },
-//                 success: function(data) {
-//                     $('#subscribe_submit').button('reset');
-//
-//                     //Use labels to display messages
-//                     //$('.error').html(data);
-//
-//                     //Use modal popups to display messages
-//                     $('#modalSubscribe .modal-title').html('<i class="icon-envelope-letter"></i>' + data);
-//                     $('#modalSubscribe').modal('show');
-//
-//                 },
-//                 error: function() {
-//                     $('#subscribe_submit').button('reset');
-//
-//                     //Use labels to display messages
-//                     //$('.error').html('Oops! Something went wrong!');
-//
-//                     //Use modal popups to display messages
-//                     $('#modalSubscribe .modal-title').html('<i class="icon-ban"></i>Oops!<br>Something went wrong!');
-//                     $('#modalSubscribe').modal('show');
-//
-//                 }
-//             });
-//
-//
-//             //Use modal popups to display messages
-//             // $('#modalSubscribe .modal-title').html('<i class="icon-key">Please enter the OTP!');
-//             // $('#modalSubscribe').modal('show');
-//         }
-//     });
+    $('#register_form').validate({
+        onfocusout: false,
+        onkeyup: false,
+        rules: {
+            name: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            address: "required",
+            contactNumber: {
+     			required: true,
+      			minlength: 8
+    		}
+        },
+        errorPlacement: function(error, element) {
+            error.insertAfter(element);
+        },
+        messages: {
+            name: "What's your name?",
+            email: {
+                required: "What's your email?",
+                email: "Please, enter a valid email"
+            },
+            address: "What's your address?",
+            contactNumber: {
+      			required: "What's your contact?",
+      			minlength: jQuery.format("At least {0} characters")
+    		}
+        },
+
+        highlight: function(element) {
+            $(element)
+            .text('').addClass('error')
+        },
+
+        success: function(element) {
+            element
+            .text('').addClass('valid')
+        }
+    });
+});
+
+//------------------------------------------------------------------------------------
+//						Modal Validation
+//------------------------------------------------------------------------------------
+
+$('#form_otp').validate({
+    onfocusout: false,
+    onkeyup: false,
+    rules: {
+        otp_number: {
+            required: true,
+            otp_number: true
+        }
+    },
+    errorPlacement: function(error, element) {
+        error.appendTo( element.closest("form"));
+    },
+    messages: {
+        otp_number: {
+            required: "We need OTP number to verify you.",
+            otp_number: "Please, enter a OTP number"
+        }
+    },
+
+    highlight: function(element) {
+        $(element)
+    },
+
+    success: function(element) {
+        element
+            .text('').addClass('valid')
+    }
 });
