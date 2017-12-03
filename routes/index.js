a8bc35f511ce446b06fdabbf03e04ff72840e6f3
Najier <najierkhan@yahoo.ca>
2017-12-03 14:05:56 -0500

    City page added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home pag. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Workiji' });
+});
+
+
+router.get('/city', function(req, res, next) {
+    res.render('city', { title: 'City' });
 });
 
 module.exports = router;
